3f36d6927e1e2f085dd7b499f9fe2f9bcb8a2c82
raikuzuriamu <kingcoconut147890@yahoo.com>
2025-11-16 16:15:31 +0500

    Quartz sync: Nov 16, 2025, 4:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -41,16 +41,16 @@ const config: QuartzConfig = {
 },
 
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
-        },
+         light: "#ffffff",        // main background
+  lightgray: "#ffffff",    // secondary background (sidebars, cards)
+  gray: "#b8b8b8",
+  darkgray: "#4e4e4e",
+  dark: "#2b2b2b",
+  secondary: "#284b63",
+  tertiary: "#84a59d",
+  highlight: "rgba(143, 159, 169, 0.15)",
+  textHighlight: "#fff23688",
+},
       },
     },
   },
