3a866b626649fd6e91e32221b2ccabab9d766619
raikuzuriamu <kingcoconut147890@yahoo.com>
2025-11-16 16:10:21 +0500

    Quartz sync: Nov 16, 2025, 4:10 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -28,17 +28,18 @@ const config: QuartzConfig = {
         code: "IBM Plex Mono",
       },
       colors: {
-        lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
-        },
+     lightMode: {
+  light: "#ffffff",        // main background
+  lightgray: "#ffffff",    // secondary background (sidebars, cards)
+  gray: "#b8b8b8",
+  darkgray: "#4e4e4e",
+  dark: "#2b2b2b",
+  secondary: "#284b63",
+  tertiary: "#84a59d",
+  highlight: "rgba(143, 159, 169, 0.15)",
+  textHighlight: "#fff23688",
+},
+
         darkMode: {
           light: "#161618",
           lightgray: "#393639",
